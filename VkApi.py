fb2de7206d7db40d7d405b97d28dca2a881faf05
OksanaSha <oksanashatalova@bk.ru>
2021-10-22 21:21:53 +0300

    find all albums and upload photos

--- VkApi.py
+++ VkApi.py
@@ -3,10 +3,14 @@ from datetime import datetime
 
 
 class VkApi:
+
+    def __init__(self, token, user_name):
+        self.token = token
+        self.user_name = user_name
+        self.id = self._get_user_id(user_name)
+
     URL = 'https://api.vk.com/method/'
     photo_likes = {}
-    def __init__(self, token):
-        self.token = token
 
     def _get_user_id(self, user_name):
         '''
@@ -25,32 +29,37 @@ class VkApi:
         else:
             return res_json['response'][0]['id']
 
-    def get_albums(self, user_name):
+    def get_all_albums(self):
         url_photos = self.URL + 'photos.getAlbums'
-        user_id = self._get_user_id(user_name)
-        if user_id:
+        if self.id:
             params = {
-                "owner_id": user_id,
+                "owner_id": self.id,
                 "access_token": self.token,
                 "v": "5.131"
             }
             res_json = requests.get(url=url_photos, params=params).json()
-            print(res_json)
             if 'error' in res_json:
                 self.print_error_msg(res_json)
             else:
-                return res_json['response']['items']
+                albums_id_and_name = {}
+                count_albums = res_json['response']['count']
+                print(f'Доступные альбомы "{self.user_name}" - {count_albums}')
+                if count_albums != 0:
+                    for album in res_json['response']['items']:
+                        album_dict = {album['id']: album['title']}
+                        albums_id_and_name.update(album_dict)
+                    return albums_id_and_name
 
-    def get_album_photos(self, user_name, album):
+
+    def _get_album_photos_inf(self, album):
         '''
         :param user_name: id or short name
         :return: [{inf_foto1}, {inf_foto2}, ...]
         '''
         url_photos = self.URL + 'photos.get'
-        user_id = self._get_user_id(user_name)
-        if user_id:
+        if self.id and album:
             params = {
-                "owner_id": user_id,
+                "owner_id": self.id,
                 "access_token": self.token,
                 "album_id": album,
                 "extended": 1,
@@ -61,6 +70,21 @@ class VkApi:
                 self.print_error_msg(res_json)
             else:
                 return res_json['response']['items']
+
+    def get_all_photos(self, album, limit):
+        photos_name_and_size = []
+        # all_urls = []
+        photos_inf = self._get_album_photos_inf(album)
+        if photos_inf:
+            if limit > len(photos_inf):
+                print(f"В альбоме {album} фотографий меньше {limit}, "
+                      f"будет загружено {len(photos_inf)} шт.")
+            for photo in photos_inf[:limit]:
+                photo_inf_dict = self.get_max_size_and_url(photo)
+                photos_name_and_size.append(photo_inf_dict)
+                # all_urls.append(photo_url)
+            return photos_name_and_size
+
 
     def _unix_date_to_date(self, unix_date):
         date = str(datetime.fromtimestamp(unix_date).date())
@@ -81,14 +105,12 @@ class VkApi:
             name = likes
 
         size = photo['sizes'][-1]['type']
-        photo_dict = {'file_name': f'{name}.jpg', 'size': size}
         url_photo = photo['sizes'][-1]['url']
-        return photo_dict, url_photo
+        photo_dict = {'file_name': f'{name}.jpg', 'size': size, 'url': url_photo}
+        return photo_dict
 
     def print_error_msg(self, response_json):
         print(f"Что-то пошло не так.\n"
               f"Код ошибки: {response_json['error']['error_code']}\n"
               f"Причина: {response_json['error']['error_msg']}")
 
-
-class VkUser(VkApi):
--- main.py
+++ main.py
@@ -14,27 +14,28 @@ def write_to_json(album, photos_inf_list):
     with open(json_name, 'w') as file:
         json.dump(data, file)
 
+def upload_photos(album_name, all_photos):
+    new_dir_yadisk = f'album_{album_name}'
+    ya_user.create_dir(new_dir_yadisk)
+    for photo in tqdm(all_photos):
+        photo_name = photo['file_name']
+        photo_url = photo.pop('url')
+        ya_user.upload_file(file_url=photo_url, name_dir=new_dir_yadisk, file_name=photo_name)
+    return all_photos
+
 
 if __name__ == '__main__':
-    limit_photos = 15
+    limit_photos = 5
     user_name = "begemot_korovin"
-    album_vk = "profile"
-    vk_user = VkApi(TOKEN_VK)
+    # user_name = 'mariacoruja'
+    album = "profile"
+    vk_user = VkApi(TOKEN_VK, user_name)
     ya_user = YaDisk(TOKEN_YADISK)
 
-    album_photos_inf = vk_user.get_album_photos(user_name, album_vk)
-    photos_name_and_size = []
-    if limit_photos > len(album_photos_inf):
-        print(f"Фотографий профиля меньше {limit_photos}, "
-              f"будет загружено {len(album_photos_inf)} шт.")
-
-    new_dir_yadisk = 'vk_profile'
-    ya_user.create_dir(new_dir_yadisk)
-
-    for photo in tqdm(album_photos_inf[:limit_photos]):
-        photo_inf_dict, photo_url = vk_user.get_max_size_and_url(photo)
-        photo_name = photo_inf_dict['file_name']
-        ya_user.upload_file(file_url=photo_url, name_dir=new_dir_yadisk, file_name=photo_name)
-        photos_name_and_size.append(photo_inf_dict)
-
-    write_to_json(album_vk, photos_name_and_size)
+    all_albums = vk_user.get_all_albums()
+    if all_albums:
+        print(all_albums)
+    # album = 228512720
+    all_album_photos = vk_user.get_all_photos(album, limit_photos)
+    for_json = upload_photos(album, all_album_photos)
+    write_to_json(album, for_json)
